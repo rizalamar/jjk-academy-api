b3a13ef27d407f576ff9e4306d1d13745f0ad970
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-05-20 23:56:21 +0700

    update data more characters added

--- src/main/java/com/jjk/jjkacademyapi/config/DataInitializer.java
+++ src/main/java/com/jjk/jjkacademyapi/config/DataInitializer.java
@@ -78,6 +78,107 @@ public class DataInitializer implements CommandLineRunner {
                 .rank("Special Grade")
                 .cursedTechnique("Mimicry / Copy")
                 .build();
+        Sorcerer yuji = Sorcerer.builder()
+                .name("Itadori Yuji")
+                .age(17)
+                .rank("Grade 1")
+                .cursedTechnique("Divergent Fist / Black Flash")
+                .build();
+        Sorcerer toge = Sorcerer.builder()
+                .name("Inumaki Toge")
+                .age(18)
+                .rank("Grade 1")
+                .cursedTechnique("Cursed Speech")
+                .build();
+        Sorcerer todo = Sorcerer.builder()
+                .name("Todo Aoi")
+                .age(19)
+                .rank("Grade 1")
+                .cursedTechnique("Boogie Woogie")
+                .build();
+        Sorcerer mei = Sorcerer.builder()
+                .name("Mei Mei")
+                .age(31)
+                .rank("Grade 1")
+                .cursedTechnique("Black Bird Manipulation")
+                .build();
+        Sorcerer geto = Sorcerer.builder()
+                .name("Suguru Geto")
+                .age(18)
+                .rank("Special Grade")
+                .cursedTechnique("Cursed Spirit Manipulation")
+                .build();
+        Sorcerer mahito = Sorcerer.builder()
+                .name("Mahito")
+                .age(0) // Representasi sebagai cursed spirit yang baru lahir
+                .rank("Special Grade")
+                .cursedTechnique("Idle Transfiguration")
+                .build();
+
+        Sorcerer jogo = Sorcerer.builder()
+                .name("Jogo")
+                .age(100) // Berusia ratusan tahun sebagai kutukan bumi
+                .rank("Special Grade")
+                .cursedTechnique("Disaster Flames")
+                .build();
+
+        Sorcerer hanami = Sorcerer.builder()
+                .name("Hanami")
+                .age(100)
+                .rank("Special Grade")
+                .cursedTechnique("Disaster Plants")
+                .build();
+
+        Sorcerer choso = Sorcerer.builder()
+                .name("Choso")
+                .age(150) // Lahir dari rahim terkutuk (Death Painting)
+                .rank("Special Grade")
+                .cursedTechnique("Blood Manipulation")
+                .clan(kamo)
+                .build();
+
+        Sorcerer sukuna = Sorcerer.builder()
+                .name("Ryomen Sukuna")
+                .age(1000) // Raja Kutukan dari era Heian
+                .rank("Special Grade")
+                .cursedTechnique("Shrine (Cleave and Dismantle)")
+                .build();
+
+        Sorcerer panda = Sorcerer.builder()
+                .name("Panda")
+                .age(16)
+                .rank("Semi-Grade 1")
+                .cursedTechnique("Abrupt Mutated Cursed Corpse")
+                .build();
+
+        Sorcerer kasumi = Sorcerer.builder()
+                .name("Kasumi Miwa")
+                .age(17)
+                .rank("Grade 3")
+                .cursedTechnique("New Shadow Style")
+                .build();
+
+        Sorcerer noritoshi = Sorcerer.builder()
+                .name("Noritoshi Kamo")
+                .age(18)
+                .rank("Semi-Grade 1")
+                .cursedTechnique("Blood Manipulation")
+                .clan(kamo)
+                .build();
+
+        Sorcerer utahime = Sorcerer.builder()
+                .name("Utahime Iori")
+                .age(31)
+                .rank("Grade 1")
+                .cursedTechnique("Solo Solo No Maai")
+                .build();
+
+        Sorcerer kusakabe = Sorcerer.builder()
+                .name("Atsuya Kusakabe")
+                .age(35)
+                .rank("Grade 1")
+                .cursedTechnique("New Shadow Style (No Innate Technique)")
+                .build();
 
         sorcererRepository.save(megumi);
         sorcererRepository.save(maki);
@@ -85,6 +186,21 @@ public class DataInitializer implements CommandLineRunner {
         sorcererRepository.save(nobara);
         sorcererRepository.save(nanami);
         sorcererRepository.save(yuta);
+        sorcererRepository.save(yuji);
+        sorcererRepository.save(todo);
+        sorcererRepository.save(toge);
+        sorcererRepository.save(mei);
+        sorcererRepository.save(geto);
+        sorcererRepository.save(mahito);
+        sorcererRepository.save(jogo);
+        sorcererRepository.save(sukuna);
+        sorcererRepository.save(panda);
+        sorcererRepository.save(kasumi);
+        sorcererRepository.save(choso);
+        sorcererRepository.save(hanami);
+        sorcererRepository.save(noritoshi);
+        sorcererRepository.save(utahime);
+        sorcererRepository.save(kusakabe);
         System.out.println("Initial data Jujutsu Kaisen");
     }
 }
