d6757e097b54584598b7e57eeadcc2f497e1953c
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-05-20 16:06:21 +0700

    add entity sorceres

--- src/main/java/com/jjk/jjkacademyapi/model/Sorcerer.java
+++ src/main/java/com/jjk/jjkacademyapi/model/Sorcerer.java
@@ -1,4 +1,27 @@
 package com.jjk.jjkacademyapi.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "sorceres")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Sorcerer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+    private Integer age;
+    private String rank;
+    private String cursedTechnique;
 }
