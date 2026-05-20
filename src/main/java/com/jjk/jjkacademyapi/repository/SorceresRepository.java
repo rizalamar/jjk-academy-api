5276a04a1f32c7d22056a4e0b0ed8ef0448181b3
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-05-20 16:09:20 +0700

    add: sorcerer repository

---
+++ src/main/java/com/jjk/jjkacademyapi/repository/SorceresRepository.java
@@ -0,0 +1,11 @@
+package com.jjk.jjkacademyapi.repository;
+
+import com.jjk.jjkacademyapi.model.Sorcerer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface SorceresRepository extends JpaRepository<Sorcerer, UUID> {
+}
